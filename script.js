500c09f46a684c64ebf868dfc2dc43e95488be59
Daniel-s-Wiliam <74251379+Daniel-s-Wiliam@users.noreply.github.com>
2020-11-10 15:54:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+  // Place date below this line
+  let dateToCount = "Nov 12, 2020 15:44:56",
+    countDown = new Date(dateToCount).getTime(),
+    x = setInterval(function () {
+      let now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+
+      //do something later when date is reached
+      if (distance < 0) {
+        let headline = document.getElementById("headline"),
+          countdown = document.getElementById("countdown"),
+          content = document.getElementById("content");
+        // -----------------------------CHANGE GOOGLE TO WHATEVER U WANT (DONT REMOVE THE QUOTATIONS)------------
+        headline.outerHTML =
+          "<h1 " +
+          "id=" +
+          '"headline"' +
+          ">" +
+          "<a " +
+          "href=" +
+          '"www.google.com"' +
+          ">" +
+          "www.google.com" +
+          "</a>" +
+          "</h1>";
+        // headline.innerText = "www.google.com";
+        countdown.style.display = "none";
+        content.style.display = "block";
+
+        clearInterval(x);
+      }
+      //seconds
+    }, 0);
+})();
